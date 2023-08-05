3c4f7a139cd547403bf19d1af70156fac4a16ac3
malauraboria <134164213+malauraboria@users.noreply.github.com>
2023-08-05 08:41:00 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+let name_apellido = prompt("Ingrese nombre y apellido")
+alert(name_apellido)
+
+if(name_apellido = "") {
+    alert("Por favor, ingrese nombre y apellido");
+}else if(name_apellido =" ") {
+    alert("Muchas gracias, puede solicitar su turno");
+}
+
