05a144213a41cda422a73c2996916ff34af1294f
malauraboria <134164213+malauraboria@users.noreply.github.com>
2023-08-05 22:04:43 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-let name_apellido = prompt("Ingrese nombre y apellido")
-alert(name_apellido)
+for (let turno = 1; turno <= 10; turno++) {
+    let nombre = prompt("Ingrese el nombre").toLowerCase();
+    alert(`Turno #${turno} Nombre: ${nombre}`);
 
-if(name_apellido = "") {
-    alert("Por favor, ingrese nombre y apellido");
-}else if(name_apellido =" ") {
-    alert("Muchas gracias, puede solicitar su turno");
+    if (!isNaN(nombre)) {
+        alert("Nombre incorrecto. No puede ser un número.");
+    }
 }
 
+alert("No hay más turnos");
+
+let turnos = new Turno("Laura", 1);
+
+function Turno(nombre, turno) {
+    this.nombre = nombre;
+    this.turno = turno;
+}
+
+let infoTurno = `Su nombre es ${turnos.nombre}, número de turno: ${turnos.turno}`;
+console.log(infoTurno);
